77af4aa0b1e5f5297159523cc9205811762ac4d8
Acetyra <rene.luck@gmx.de>
2020-06-05 18:51:08 +0200

    fixed wifi
    @derstanlay nach einem durchlauf steht immer noch der wert von voher drinnen, der wird dann überschrieben. steht jedoch etwas 3 stelliges drinnen, dannach 2 stellig dann bleibt die letzte stelle erhalten

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,28 @@
 #include "WiFi.h"
 #include <FastLED.h>
 
-const char * ssid           = "MPL";
-const char * password       = "123456789";
-String       client_address = "C01";
+const char *ssid = "MPL";
+const char *password = "123456789";
+String client_address = "C01";
 
+#define LED_PIN 15
+#define POWER 4
+#define NUM_LEDS 144
 
-char befehl[10];
-
-#define LED_PIN     15
-#define POWER       4
-#define NUM_LEDS    144
-
-#define LED_TYPE    WS2812
+#define LED_TYPE WS2812
 #define COLOR_ORDER GRB
 
 int Brightness = 0;
 
 int status = WL_IDLE_STATUS;
 unsigned int localPort = 3333;
-char packetBuffer[255];
-int packetSize = 0;
+
 unsigned int ledHight = 0;
 WiFiUDP Udp;
 
 CRGBArray<NUM_LEDS> ledsOld;
 CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
+TBlendType currentBlending;
 
 void setup()
 {
@@ -36,7 +32,8 @@ void setup()
   Serial.begin(115200);
   currentPalette = RainbowColors_p;
   currentBlending = LINEARBLEND;
-  while (status != WL_CONNECTED) {
+  while (status != WL_CONNECTED)
+  {
 
     status = WiFi.begin(ssid, password);
     Serial.println("Verbindungsversuch");
@@ -45,11 +42,8 @@ void setup()
 
   Serial.println("Verbunden");
 
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(ledsOld, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(ledsOld, NUM_LEDS).setCorrection( TypicalLEDStrip );
-
-  
-  
   Brightness = 255;
   FastLED.setBrightness(Brightness);
 
@@ -57,26 +51,26 @@ void setup()
   //FastLED.show();
   Udp.begin(localPort);
   digitalWrite(POWER, HIGH);
- 
 }
 
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
+void FillLEDsFromPaletteColors(uint8_t colorIndex)
 {
-    uint8_t brightness = 255;
-    
-    for( int i = 0; i < ledHight; i++) {
-        ledsOld[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
-    }
-}
+  uint8_t brightness = 255;
 
+  for (int i = 0; i < ledHight; i++)
+  {
+    ledsOld[i] = ColorFromPalette(currentPalette, colorIndex, brightness, currentBlending);
+    colorIndex += 3;
+  }
+}
 
 void loop()
 {
 
-  packetSize = Udp.parsePacket();
+  int packetSize = Udp.parsePacket();
   if (packetSize)
   {
+    char packetBuffer[255];
     //Serial.print("Received packet of size ");
     //Serial.println(packetSize);
     //Serial.print("From ");
@@ -86,7 +80,7 @@ void loop()
     //Serial.println(Udp.remotePort());
 
     // read the packet into packetBufffer
-    int len = Udp.read(befehl, 10);
+    int len = Udp.read(packetBuffer, 255);
     //if (len > 0) {
     //  packetBuffer[len] = 0;
     //}
@@ -96,13 +90,12 @@ void loop()
     ledHight = atoi(packetBuffer);
     FastLED.clear();
     Serial.println(ledHight);
-    if(ledHight > 144)
+    if (ledHight > 144)
     {
       ledHight = 144;
     }
-    fill_rainbow (ledsOld, ledHight, CRGB(255, 255, 255));
+    fill_rainbow(ledsOld, ledHight, CRGB(255, 255, 255));
     //FillLEDsFromPaletteColors(1);
     FastLED.show();
   }
 }
-
